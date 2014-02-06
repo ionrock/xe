89c26ed8937abc7d72737da22ec282c1b9f9cea6
Eric Larson <eric@ionrock.org>
2014-02-06 16:19:33 -0600

    Use config to define the django manage.py file

--- xe/cmds/manage.py
+++ xe/cmds/manage.py
@@ -1,10 +1,12 @@
 from path import path
+from xe import settings
 from xe.do import env_do
 
 
 def main(args):
-    if not path('manage.py').exists():
+    manage = path(settings['DJANGO_MANAGE'])
+    if not manage.exists():
         print('No manage.py exists in the root directory')
         return
 
-    env_do('python manage.py %s' % ' '.join(args))
+    env_do('python %s %s' % (manage,' '.join(args)))
